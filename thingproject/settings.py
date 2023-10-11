023757d7d027f9e24f65cb571dca67c1cb32dc0c
Yifei Shi <yifei.shi@kcl.ac.uk>
2023-10-12 06:15:21 +0800

    Reregistered the app

--- thingproject/settings.py
+++ thingproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'things',
+    'things.apps.ThingsConfig',
 ]
 
 MIDDLEWARE = [
