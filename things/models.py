e7b9a7d803e672607c11157de56702679ff5740e
kiyoaki <yifei.shi@kcl.ac.uk>
2023-10-12 05:59:05 +0800

    Simple edits on some value

--- things/models.py
+++ things/models.py
@@ -4,8 +4,9 @@ from django.db import models
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
-    description = models.CharField(max_length=120, blank=True)
+    description = models.CharField(max_length=120, unique=False, blank=True)
     quantity = models.IntegerField(
+        unique=False,
         validators=[
             MaxValueValidator(100),
             MinValueValidator(0)
--- things/views.py
+++ things/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def show(request):
-    return HttpResponse(request, "things.html")
+    return render(request, "things.html")
